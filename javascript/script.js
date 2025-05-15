753b9785efeb250d06d85337ed4bc55e889db542
Putri1606 <Putrimawarfadilah@gmail.com>
2025-05-15 20:10:50 +0700

    js

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+let number1 = 10
+let number2 = 20
+let result = number1 + number2
+
+let add = number1 + number2
+console.log(add)
+
+let sub = number1 - number2
+console.log(sub)
+
+let multi = number1 * number2
+console.log(multi)
+
+let div = number1 / number2
+console.log(div)
+
+let exp = 3 ** 4
+console.log(exp)
+
+let mod = 10 % 3 // sisa bagi dari 10 bagi 3
+console.log(mod)
